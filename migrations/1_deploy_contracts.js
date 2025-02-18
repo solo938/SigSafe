19f091c99f6402e943aa16599eb3d3a8548ca34d
Sahariar Hasan <68711966+solo938@users.noreply.github.com>
2025-02-18 13:14:11 +0530

    Update 1_deploy_contracts.js

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,1 +1,10 @@
+const MultiSigWallet = artifacts.require("MultiSigWallet");
+
+module.exports = function(deployer) {
+  // List of owners (replace with actual Ethereum addresses)
+  const owners = ["0xOwnerAddress1", "0xOwnerAddress2", "0xOwnerAddress3"];
+  const numConfirmationsRequired = 2; // Number of confirmations required to execute a transaction
+
+  deployer.deploy(MultiSigWallet, owners, numConfirmationsRequired);
+};
 
